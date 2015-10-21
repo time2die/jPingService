5b1504c262fc1b743aa0aeb84663cdb05593f332
alex <time2rfc@gmail.com>
2015-10-21 21:13:55 +0300

    add request class

---
+++ src/main/java/org/time2java/jpingservice/Request.java
@@ -0,0 +1,16 @@
+package org.time2java.jpingservice;
+
+import java.util.Date;
+
+/**
+ *
+ * @author time2java
+ */
+public class Request {
+    RequestStatus status ;
+    String host ;
+    String port ;
+    String path ;
+    String reply ;
+    Date date ;
+}
