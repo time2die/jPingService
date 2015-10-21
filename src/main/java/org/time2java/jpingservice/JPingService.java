97d42547f559fda071caf8a327c2184859ca1dfe
alex <time2rfc@gmail.com>
2015-10-21 21:03:13 +0300

    test in\ouput

--- src/main/java/org/time2java/jpingservice/JPingService.java
+++ src/main/java/org/time2java/jpingservice/JPingService.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.time2java.jpingservice;
 
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- *
  * @author alex
  */
 public class JPingService {
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        th.start();
+        
+        while (scanner.hasNextLine()) {
+            String s = scanner.nextLine();
+        }
+        
+    }
+
+    static Thread th = new Thread() {
+
+        @Override
+        public void run() {
+            int i = 0;
+            while (i < 200) {
+                try {
+                    i++;
+                    System.out.println("stop");
+                    System.out.println("the");
+                    System.out.println("fuck");
+                    Thread.sleep(4000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(JPingService.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    };
+
 }
