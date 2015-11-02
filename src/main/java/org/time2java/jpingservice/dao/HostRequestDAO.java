511bedc7f4d8a39a0870c41cb31caf1d8d32aac6
alex <time2rfc@gmail.com>
2015-11-02 17:06:02 +0300

    close session and right work with entity

--- src/main/java/org/time2java/jpingservice/dao/HostRequestDAO.java
+++ src/main/java/org/time2java/jpingservice/dao/HostRequestDAO.java
@@ -1,5 +1,6 @@
 package org.time2java.jpingservice.dao;
 
+import java.util.List;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -8,6 +9,7 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.time2java.jpingservice.HostRequest;
+import org.time2java.jpingservice.RequestStatus;
 
 /**
  * @author time2java
@@ -35,57 +37,104 @@ public class HostRequestDAO {
 
     public HostRequestDAO() {
         sessionFactory = new Configuration().configure().buildSessionFactory();
-        session = sessionFactory.openSession();
+        
     }
 
     public void addOrUpdateRequest(HostRequest request) {
-        HostRequest oldRequest = null ;
-        try{
-        oldRequest = (HostRequest) session.createCriteria(HostRequest.class).add(Restrictions.eq("host", request.getHost()))
-                                                .add(Restrictions.eq("port", request.getPort()))
-                                                .add(Restrictions.eq("path", request.getPath())).addOrder(Order.desc("date")).uniqueResult();
-        }catch(HibernateException ex){
+        session = sessionFactory.openSession();
+        
+        HostRequest oldRequest = null;
+        try {
+            oldRequest = (HostRequest) session.createCriteria(HostRequest.class)
+                    .add(Restrictions.eq("host", request.getHost()))
+                    .add(Restrictions.eq("port", request.getPort()))
+                    .add(Restrictions.eq("path", request.getPath()))
+                    .add(Restrictions.eq("status", request.getStatus()))
+                    .uniqueResult();
+        } catch (HibernateException ex) {
             ex.printStackTrace();
         }
 
-        Transaction tx =  session.beginTransaction();
+        Transaction tx = session.beginTransaction();
         
-        if(oldRequest != null){
-            oldRequest.setCode(request.getCode());
-            oldRequest.setDate(request.getDate());
-            oldRequest.setReply(request.getReply());
-            oldRequest.setStatus(request.getStatus());
-            System.out.println("update: " + oldRequest);
-        }else{
-            oldRequest = request ;
-            System.out.println("save: " + request);
+        if (oldRequest != null) {
+            HostRequest dbRequest = session.load(HostRequest.class, oldRequest.getId()) ;
+            
+            dbRequest.setCode(request.getCode());
+            dbRequest.setDate(request.getDate());
+            dbRequest.setReply(request.getReply());
+            dbRequest.setStatus(request.getStatus());
+            System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>update: " + oldRequest);
+            session.merge(dbRequest);
+        } else {
+            System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>save: " + request);
+            session.save(request);
         }
 
-        session.saveOrUpdate(request);
-        tx.commit(); 
+//        session.saveOrUpdate(request);
+        tx.commit();
+        session.flush();
+        session.close() ;
     }
 
-    public HostRequest getRequest(HostRequest lookingFor) {
-        
-        HostRequest oldRequest = null ;
-        try{
-        oldRequest = (HostRequest) session.createCriteria(HostRequest.class).add(Restrictions.eq("host", lookingFor.getHost()))
-                                                .add(Restrictions.eq("port", lookingFor.getPort()))
-                                                .add(Restrictions.eq("path", lookingFor.getPath())).addOrder(Order.desc("date")).uniqueResult();
-        }catch(HibernateException ex){
+    public HostRequest getAddedRequest(HostRequest lookingFor) {
+        session = sessionFactory.openSession();
+        HostRequest oldRequest = null;
+        try {
+            oldRequest = (HostRequest) session.createCriteria(HostRequest.class)
+                    .add(Restrictions.eq("host", lookingFor.getHost()))
+                    .add(Restrictions.eq("port", lookingFor.getPort()))
+                    .add(Restrictions.eq("path", lookingFor.getPath()))
+                    .add(Restrictions.eq("status", RequestStatus.ADDED))
+                    .addOrder(Order.desc("date")).uniqueResult();
+        } catch (HibernateException ex) {
             ex.printStackTrace();
+        }finally{
+            session.close(); 
         }
-        
+
         return oldRequest;
     }
 
-    private void getStatus(int id) throws Exception {
-        HostRequest surveyInSession = (HostRequest) session.get(HostRequest.class, Long.valueOf(id));
-        System.out.println(surveyInSession.getHost());
+    public List<HostRequest> getAllAdededRequest() {
+        try {
+            session = sessionFactory.openSession();
+            return (List<HostRequest>) session.createCriteria(HostRequest.class)
+                    .add(Restrictions.eq("status", RequestStatus.ADDED))
+                    .addOrder(Order.desc("date"));
+        } finally {
+            session.close();
+        }
     }
 
+    public HostRequest getRequest(HostRequest lookingFor) {
+        session = sessionFactory.openSession() ;
+        HostRequest oldRequest = null;
+        try {
+            oldRequest = (HostRequest) session.createCriteria(HostRequest.class).add(Restrictions.eq("host", lookingFor.getHost()))
+                    .add(Restrictions.eq("port", lookingFor.getPort()))
+                    .add(Restrictions.eq("path", lookingFor.getPath()))
+                    .add(Restrictions.eq("status", lookingFor.getStatus()))
+                    .addOrder(Order.desc("date")).uniqueResult();
+        } catch (HibernateException ex) {
+            ex.printStackTrace();
+        }finally{
+            session.close();
+        }
+
+        return oldRequest;
+    }
+
+//    private void getStatus(int id) throws Exception {
+//        HostRequest surveyInSession = (HostRequest) session.get(HostRequest.class, add ya.ru Long.valueOf(id));
+//        System.out.println(surveyInSession.getHost());
+//    }
+
     public void close() {
-        session.close();
+        if(session.isConnected()){
+            session.close();
+        }
+        
         sessionFactory.close();
     }
 }
