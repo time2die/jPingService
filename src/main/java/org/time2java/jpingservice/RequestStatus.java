39bc88a232cc6ceff8aa9d0d4fccfb861104d9da
alex <time2rfc@gmail.com>
2015-11-02 17:05:07 +0300

    delete NEW code

--- src/main/java/org/time2java/jpingservice/RequestStatus.java
+++ src/main/java/org/time2java/jpingservice/RequestStatus.java
@@ -5,7 +5,6 @@ package org.time2java.jpingservice;
  * @author time2java
  */
 public enum RequestStatus {
-    NEW,        // только считали
     ADDED,      // добавили в потом обработки
     FINISHED;   // результат известен
 }
