dcd8551175b95c0f17d27d5e4f0667020288da90
alex <time2rfc@gmail.com>
2015-11-02 13:20:06 +0300

    add right dao code

--- src/main/java/org/time2java/jpingservice/dao/HostRequestDAO.java
+++ src/main/java/org/time2java/jpingservice/dao/HostRequestDAO.java
@@ -1,14 +1,19 @@
 package org.time2java.jpingservice.dao;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 import org.time2java.jpingservice.HostRequest;
 
 /**
  * @author time2java
  */
 public class HostRequestDAO {
+
     Session session;
 
     //double check singlton impl
@@ -26,56 +31,61 @@ public class HostRequestDAO {
         }
         return localInstance;
     }
-    private SessionFactory sessionFactory ;
+    private SessionFactory sessionFactory;
+
     public HostRequestDAO() {
-         sessionFactory = new Configuration().configure().buildSessionFactory();
+        sessionFactory = new Configuration().configure().buildSessionFactory();
         session = sessionFactory.openSession();
     }
 
-    public void updateRequest(HostRequest request) {
-        System.out.println("save: "+ request);
+    public void addOrUpdateRequest(HostRequest request) {
+        HostRequest oldRequest = null ;
+        try{
+        oldRequest = (HostRequest) session.createCriteria(HostRequest.class).add(Restrictions.eq("host", request.getHost()))
+                                                .add(Restrictions.eq("port", request.getPort()))
+                                                .add(Restrictions.eq("path", request.getPath())).addOrder(Order.desc("date")).uniqueResult();
+        }catch(HibernateException ex){
+            ex.printStackTrace();
+        }
+
+        Transaction tx =  session.beginTransaction();
+        
+        if(oldRequest != null){
+            oldRequest.setCode(request.getCode());
+            oldRequest.setDate(request.getDate());
+            oldRequest.setReply(request.getReply());
+            oldRequest.setStatus(request.getStatus());
+            System.out.println("update: " + oldRequest);
+        }else{
+            oldRequest = request ;
+            System.out.println("save: " + request);
+        }
+
+        session.saveOrUpdate(request);
+        tx.commit(); 
     }
-    
-    public HostRequest getRequest(HostRequest lookingFor){
-        return new HostRequest() ;
+
+    public HostRequest getRequest(HostRequest lookingFor) {
+        
+        HostRequest oldRequest = null ;
+        try{
+        oldRequest = (HostRequest) session.createCriteria(HostRequest.class).add(Restrictions.eq("host", lookingFor.getHost()))
+                                                .add(Restrictions.eq("port", lookingFor.getPort()))
+                                                .add(Restrictions.eq("path", lookingFor.getPath())).addOrder(Order.desc("date")).uniqueResult();
+        }catch(HibernateException ex){
+            ex.printStackTrace();
+        }
+        
+        return oldRequest;
     }
 
     private void getStatus(int id) throws Exception {
         HostRequest surveyInSession = (HostRequest) session.get(HostRequest.class, Long.valueOf(id));
         System.out.println(surveyInSession.getHost());
     }
-    
-    public void close(){
+
+    public void close() {
+        session.close();
         sessionFactory.close();
     }
-
-
-//    HibernateUtil hibernateUtil = new HibernateUtil();
-//        
-//    Session session = hibernateUtil.getSession();
-//    session.beginTransaction();
-//
-//    HostRequest ya = new HostRequest();
-//    ya.setHost("ya");
-//    ya.setPort(80);
-//    ya.setPath("/");
-//    ya.setStatus(RequestStatus.NEW);
-//    
-//    
-//    session.save(ya);
-//    session.flush();
-//    System.out.println(">"+ya.getId());
-//    
-//    ya = new HostRequest() ;
-//    ya.setHost("google.com");
-//    session.save(ya);
-//    session.flush();
-//    
-//    System.out.println(">>"+ya.getId());
-//    
-//    HostRequest surveyInSession = (HostRequest) session.get(HostRequest.class, ya.getId());
-//    System.out.println(surveyInSession.getHost());
-//
-//    session.getTransaction().commit(); 
-//    session.close();
 }
